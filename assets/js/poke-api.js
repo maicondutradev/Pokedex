08890c5bac6cd8d8daa3bebec7a0b9abaa51119d
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-01-02 09:20:10 -0300

    docs: #4 Convertendo o modelo do PokeApi para o meu modelo

---
+++ assets/js/poke-api.js
@@ -0,0 +1,17 @@
+const PokeApi = {}
+
+PokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url).then((response) => response.json())
+}
+
+PokeApi.getPokemons = (offset = 0, limit = 10) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
+
+    return fetch(url)
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(PokeApi.getPokemonDetail))
+        .then((detailsRequest) => Promise.all(detailsRequest))
+        .then((pokemonsDetails) => pokemonsDetails)
+}
+
